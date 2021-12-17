9f404134468c6e2774ae405fcbbe9f62a7c3adb6
tkyg <80114433+tkyg@users.noreply.github.com>
2021-12-17 18:40:19 -0500

    commit tree 2c4e030aec2f77ddc4b8595ccc50f33c7509f78c

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ input.addEventListener('click', function() {
   alert('I was clicked!');
 });
 }
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
